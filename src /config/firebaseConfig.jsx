b716aa0d1ae32433254fdeb445a99669d3b491d8
varsha <149252098+varsha24v@users.noreply.github.com>
2024-08-10 18:17:53 -0400

    Create firebaseConfig.jsx

---
+++ src /config/firebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlpD34rLhkN8kClJnOdbXIdnS9PnKWuzg",
+  authDomain: "seeds-studysync.firebaseapp.com",
+  projectId: "seeds-studysync",
+  storageBucket: "seeds-studysync.appspot.com",
+  messagingSenderId: "356155208251",
+  appId: "1:356155208251:web:642b4e77b5693fd1bc221e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
